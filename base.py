5afb7eef62e682c9f61c15cacde19738a50c5637
seon <sunkyoung.dev@gmail.com>
2024-01-23 13:46:43 +0900

    base.py(전역상수 설정관련파일)

---
+++ base.py
@@ -0,0 +1,13 @@
+# 전역적으로 사용할 사용자 정의 변수들을 저장하는 파일
+# myproject/settings/base.py
+import os
+# Django의 기본 설정 가져오기
+from django.conf import settings
+# Django 기본 설정 상속
+DATABASES = settings.DATABASES
+DEBUG = settings.DEBUG
+
+BASIC_PATH=os.getcwd()
+AUDIO_DOWN_PATH=BASIC_PATH+'/download'
+
+# ... 기타 설정을 필요에 따라 상속
