36e40b3d7e2cdabb28450a848c8af34317a8321e
seon <darkness.blooms.sun@gmail.com>
2024-01-26 20:31:46 +0900

    경로수정2

--- notation/transfet_utils/transfer.py
+++ notation/transfet_utils/transfer.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.conf import settings
 #전역변수 관련 라이브러리
-from base import BASIC_PATH,AUDIO_DOWN_PATH
+from base import BASIC_PATH,AUDIO_DOWN_PATH,AUDIO_MIDI_FILE
 
 from notation.transfet_utils import down_mp3,mp3_to_midi,midi_to_xml,xml_to_pdf
 
@@ -24,7 +24,7 @@ def transfer_process(url):
     # JsonResponse({'error': '유트브 정책상문제로 다운로드 할수 없습니다. 다른 음원을 이용해보세요!'}, status=405)
     
     try:
-        midi_path=mp3_to_midi.down_musicxml(audio_path,AUDIO_DOWN_PATH)
+        midi_path=mp3_to_midi.down_musicxml(audio_path,AUDIO_DOWN_PATH,AUDIO_MIDI_FILE)
         print('midipath',midi_path)
     except:
         return JsonResponse({'error': 'midi파일로 변환중 문제입니다. 서버문제 입니다'}, status=500)
