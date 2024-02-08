76245719809a6e2bb1994980b5e37ab2d438b55a
seon <darkness.blooms.sun@gmail.com>
2024-02-08 13:27:22 +0900

    delete 로직 변경

--- notation/utils/delete.py
+++ notation/utils/delete.py
@@ -1,24 +1,27 @@
 import os
 from django.conf import settings
+import shutil
 #전역변수 관련 라이브러리
 from base import BASIC_PATH,AUDIO_DOWN_PATH
-def delete_files_in_folder(file_path):
-    print('이거 돌아감..?')
-    try:
-        # 폴더 내 모든 파일 가져오기
-        # files = os.listdir(AUDIO_DOWN_PATH)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-            print(f"{file_path} deleted")
-        # # 각 파일을 삭제
-        # for file in files:
-        #     file_path = os.path.join(AUDIO_DOWN_PATH, file)
-        #     if os.path.isfile(file_path):
-        #         os.remove(file_path)
-        #         print(f"{file_path} deleted")
+def delete_files_in_folder(path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            file_path = os.path.join(root, name)
+            try:
+                os.remove(file_path)
+                print("Deleted file:", file_path)
+            except Exception as e:
+                print("Failed to delete file:", file_path, "Error:", e)
+        for name in dirs:
+            dir_path = os.path.join(root, name)
+            try:
+                shutil.rmtree(dir_path)
+                print("Deleted directory:", dir_path)
+            except Exception as e:
+                print("Failed to delete directory:", dir_path, "Error:", e)
 
         print("All files deleted successfully.")
 
-    except Exception as e:
-        print(f"Error: {e}")
+    # except Exception as e:
+    #     print(f"Error: {e}")
 
