2acefdda2df08dabc1330e96dd8025ce1d0a76b3
seon <darkness.blooms.sun@gmail.com>
2024-02-14 21:20:32 +0900

    세팅
    파이썬 애니웨어 허용

--- music/settings.py
+++ music/settings.py
@@ -150,4 +150,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8181",  # 허용할 Origin들을 명시
     "https://yourfrontenddomain.com",
+    '.pythonanywhere.com',
 ]
